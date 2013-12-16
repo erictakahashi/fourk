ac7c74ba74354e6af1381d062e4513f139d888ef
blanko <eric@blanko.be>
2013-12-16 11:14:32 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fourk::Application.config.secret_key_base = '6ec7dc5c4ef3d4c50ce4c181381917f63932b903f94689ae36636e871811443f7564fa5076a71bfda1f60c6e82f3877e18d5890aeaffbe8bf0564bba542aefdc'
