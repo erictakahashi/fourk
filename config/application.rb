204b0bc128f06a3a43a9d115c0dfe2ead82cded0
Eric <erictakahashi@gmail.com>
2013-12-28 15:17:37 -0200

    configuration of the bootstrap and modifications on application.html.erb

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Fourk
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :"pt-BR"
+
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
