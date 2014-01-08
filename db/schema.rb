d5b619828f0d8ff37e92b54d7ddae4475988aa9e
Eric <erictakahashi@gmail.com>
2014-01-08 04:14:37 -0200

    Migração da tabela foods

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218013449) do
+ActiveRecord::Schema.define(version: 20140108053808) do
 
   create_table "establishments", force: true do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20131218013449) do
     t.integer  "zipcode"
     t.string   "city"
     t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "foods", force: true do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
