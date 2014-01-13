cfbe970f3a3a3c621d2d13922387a2088d76e4c6
Eric <erictakahashi@gmail.com>
2014-01-13 02:40:45 -0200

    Criação de métodos para auxiliar no relacionamento entre establishment e food

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -2,4 +2,14 @@ class Establishment < ActiveRecord::Base
   validates :name, :address, :city, presence: true
 
   has_and_belongs_to_many :foods
+
+  # Check if a establishment is enrolled with a food
+  def enrolled_in?(food)
+    self.foods.include?(food)
+  end
+
+  # Return foods that is not enrolled with the establishment
+  def unenrolled_foods
+    Food.all - self.foods
+  end
 end
