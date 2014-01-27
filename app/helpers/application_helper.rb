554600e167c67650d8bc8d191881e1faf4a9aea4
Eric <erictakahashi@gmail.com>
2014-01-27 02:00:41 -0200

    Criação do helper new_rate para substituir o formulario de cadastro de notas(rates)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     content_for :title, title
     content_tag(:h2, content_tag(:small, title))
   end
+
+  def new_rate(rateable, establishment)
+    raw render(:partial => "rates/new_rate", :locals => { :rateable => rateable, :establishment => establishment })
+  end
 end
