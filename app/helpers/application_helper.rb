48f16eb7f6501010e08d93db0c2d49d59ce18c38
Eric <erictakahashi@gmail.com>
2014-01-28 00:25:30 -0200

    Criação de um método helper para exibir os formulário de cadastro de preços

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,10 @@ module ApplicationHelper
     content_tag(:h2, content_tag(:small, title))
   end
 
+  def new_price(food, establishment)
+    raw render(:partial => "prices/new_price", :locals => { :food => food, :establishment => establishment })
+  end
+
   def new_rate(rateable, establishment)
     raw render(:partial => "rates/new_rate", :locals => { :rateable => rateable, :establishment => establishment })
   end
