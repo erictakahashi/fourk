4b2cfafd0a523735bf6a8cedb113649603ebc5af
Eric <erictakahashi@gmail.com>
2014-02-04 16:23:31 -0200

    Correção do link redirecionado após o login no facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     #redirect_to root_url
-    redirect_to establishments
+    redirect_to establishments_path
   end
 
   def destroy
